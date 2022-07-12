9771b237c6b30a29882654bea3fe253a88ee40c1
MahmoudWesam <mahmoudamer95.bird@gmail.com>
2022-07-12 20:41:51 +0200

    feat: country model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,6 +5,17 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=60)
+    code = models.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
 class Address(models.Model):
     street = models.CharField(max_length=60)
     postal_code = models.CharField(max_length=10)
@@ -39,6 +50,7 @@ class Book(models.Model):
     )
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args={self.slug})
