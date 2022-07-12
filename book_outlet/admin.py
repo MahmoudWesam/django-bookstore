5bcf15fb11ae9b7dd09b2c70688565a0dd210b5f
MahmoudWesam <mahmoudamer95.bird@gmail.com>
2022-07-12 15:18:43 +0200

    initial commit

---
+++ book_outlet/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Book
+
+# Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("rating",)
+
+
+admin.site.register(Book, BookAdmin)
